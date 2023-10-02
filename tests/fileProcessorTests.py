edc8ba2250e2f7570808883393cc040326a6d0e3
Everton Martins <engeverton@gmail.com>
2023-10-02 18:47:22 -0300

    Commit: atualizado o fileProcessorTests.py

--- tests/fileProcessorTests.py
+++ tests/fileProcessorTests.py
@@ -5,3 +5,20 @@ Dt. 2023-09-29
 version 1.0.0
 
 """
+
+import unittest
+from src.fileProcessor import processZip
+
+class fileProcessorTests(unittest.TestCase):
+    
+    def test_processZip(self):
+        
+        zip_path  = ''
+        zip_name  = ''
+        val_names = ['Project', 'XMLFile', 'Line', 'FlowNameCount', 'ContextDescribe', 'FolderPath', 'FilePath']
+        
+        result = processZip(zip_path, zip_name, val_names)
+        
+        self.assertIsInstance(result,list)
+        self.assertGreater(len(result),0)
+        
